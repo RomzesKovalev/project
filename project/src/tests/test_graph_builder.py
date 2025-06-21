42f326457eaae5fcec3d21c806814f437d759f4a
RomzesKovalev <165251650+RomzesKovalev@users.noreply.github.com>
2025-06-21 14:59:55 +0300

    Update test_graph_builder.py

--- project/src/tests/test_graph_builder.py
+++ project/src/tests/test_graph_builder.py
@@ -6,17 +6,15 @@ logger = logging.getLogger(__name__)
 
 class TestGraphBuilder(unittest.TestCase):
     def setUp(self):
-        self.plotter = RealTimePlotter(interval=100, max_points=5)  # Короткий интервал для тестирования
+        self.plotter = RealTimePlotter(interval=100, max_points=5)
 
     def test_update_plot(self):
-        # Добавляем данные
         self.plotter.update_plot(None)
         self.plotter.update_plot(None)
         self.plotter.update_plot(None)
         self.plotter.update_plot(None)
         self.plotter.update_plot(None)
 
-        # Проверяем, что данные добавлены корректно
         self.assertEqual(len(self.plotter.x_data), 5, "x_data should have 5 points")
         self.assertEqual(len(self.plotter.cpu_y_data), 5, "cpu_y_data should have 5 points")
         self.assertEqual(len(self.plotter.memory_y_data), 5, "memory_y_data should have 5 points")
@@ -24,7 +22,6 @@ class TestGraphBuilder(unittest.TestCase):
 
         logger.info("Tested update_plot with 5 points")
 
-        # Добавляем ещё одну точку и проверяем ограничение на максимальное количество точек
         self.plotter.update_plot(None)
         self.assertEqual(len(self.plotter.x_data), 5, "x_data should still have 5 points after adding one more")
         self.assertEqual(len(self.plotter.cpu_y_data), 5, "cpu_y_data should still have 5 points after adding one more")
